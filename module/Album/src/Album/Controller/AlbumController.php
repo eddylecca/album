0338a880dc769b98738d8b7d501dbc5872375c07
eddylecca <eddy.lecca3@gmail.com>
2014-06-24 22:47:13 -0500

    Creado el modulo  para la base de datos

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -30,10 +30,12 @@ class AlbumController extends AbstractActionController
     // Agregamos este método
     public function getAlbumTable()
     {
+        /*
         if (!$this->albumTable) {
             $sm = $this->getServiceLocator();
             $this->albumTable = $sm->get('Album\Model\AlbumTable');
         }
         return $this->albumTable;
+         */
     }    
 }
