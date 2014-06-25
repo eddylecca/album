338ce25fbfa1daf14b75f820f25b5ef4fe95a5a8
eddylecca <eddy.lecca3@gmail.com>
2014-06-24 22:10:53 -0500

    Creado el modulo  para la base de datos

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -9,7 +9,7 @@ class AlbumController extends AbstractActionController
     public function indexAction()
     {
         return new ViewModel(array(
-            'albums' => $this->getAlbumTable()->fetchAll(),
+            'album' => $this->getAlbumTable()->fetchAll(),
         ));
  
     }
